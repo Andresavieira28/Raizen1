7bd6e05beb9c8d717fe910fc9d9315541243eafb
AghathaK <aghatha.kriss01@gmail.com>
2022-11-29 01:14:17 -0300

    template cnpj finalizado

---
+++ src/data/company.js
@@ -0,0 +1,10 @@
+export default {
+    "infos": [
+    {
+        "lote": "123456",
+        "localização": "São Paulo",
+        "procedência": "Area Fazenda Curralinho",
+        "açúcar": "tipo 1",
+        "data": "21/07/2019",
+    }
+]}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import finalCostumer from './pages/finalCostumer/finalCostumer.js';
-// import teamRaizen from './pages/teamRaizen/teamRaizen.js';
-// import searchCNPJ from './pages/SearchCNPJ/search.js';
+import searchCompany from './pages/searchCompany/searchCompany.js';
 
 const main = document.querySelector('#root');
 
@@ -11,11 +10,11 @@ const routes = () => {
       case '#finalCostumer':
         main.appendChild(finalCostumer());
         break;
+        case '#searchCompany':
+          main.appendChild(searchCompany());
+          break;
       case '#teamRaizen':
         main.appendChild(teamRaizen());
-        break;
-        case '#searchCNPJ':
-        main.appendChild(searchCNPJ());
         break;
     }
   });
