05a53663205794493c7ce2400e4c1870bb5cd9a5
AngelMelo12 <angellmello1993@gmail.com>
2022-11-28 23:15:03 -0300

    criação do template da tela para consumidor final

--- src/data/products.js
+++ src/data/products.js
@@ -1,9 +1,10 @@
-export default [
+export default {
+    "products": [
     {
         "image": "https://imagens.gimba.com.br/objetosmidia/ExibirObjetoMidia?Id=120513",
         "origin": "Minas Gerais",
         "farm": "Area Fazenda Curralinho e Ouro Preto",
         "traceability": "CURRALINHO, Santana de Pirapama - MG",
         "certification": "BONSUCRO",
-    },
-]
+    }
+]}
