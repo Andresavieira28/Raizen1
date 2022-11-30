1718f4a39a415ade9893e07441915397c16e8e45
AghathaK <aghatha.kriss01@gmail.com>
2022-11-29 21:18:09 -0300

    filtro de cnpj funcionando

--- src/data/company.js
+++ src/data/company.js
@@ -1,10 +1,49 @@
 export default {
     "infos": [
-    {
-        "lote": "123456",
-        "localização": "São Paulo",
-        "procedência": "Area Fazenda Curralinho",
-        "açúcar": "tipo 1",
-        "data": "21/07/2019",
-    }
-]}
+        {
+            "cnpj": "24409413",
+            "empresa": "24.409.413/0001-92",
+            "lote": "121212",
+            "localização": "São Paulo",
+            "procedência": "Fazenda Curralinho",
+            "açúcar": "tipo 1",
+            "data": "21/07/2019",
+        },
+        {
+            "cnpj": "35751872",
+            "empresa": "35.751.872/0001-76",
+            "lote": "131313",
+            "localização": "Curitiba",
+            "procedência": "Fazenda São José",
+            "açúcar": "tipo 3",
+            "data": "27/09/2022",
+        },
+        {
+            "cnpj": "54599204",
+            "empresa": "54.599.204/0001-10",
+            "lote": "141414",
+            "localização": "Santa Catarina",
+            "procedência": "Fazenda Bergamota",
+            "açúcar": "tipo 2",
+            "data": "13/02/2018",
+        },
+        {
+            "cnpj": "48409035",
+            "empresa": "48.409.035/0001-05",
+            "lote": "151515",
+            "localização": "Minas Gerais",
+            "procedência": "Fazenda Recanto da felicidade",
+            "açúcar": "tipo 4",
+            "data": "02/12/2021",
+        },
+        {
+            "cnpj": "26071232",
+            "empresa": "26.071.232/0001-24",
+            "lote": "161616",
+            "localização": "Espirito Santo",
+            "procedência": "Fazenda Pomar",
+            "açúcar": "tipo 2",
+            "data": "17/07/2020",
+        },
+    ]
+}
--- src/pages/searchCompany/searchCompany.js
+++ src/pages/searchCompany/searchCompany.js
@@ -2,15 +2,43 @@ import data from '../../data/company.js';
 
 const companies = data.infos;
 
-function createCard(companies) {
-    const companiesArray = companies.map((infos) => {
+export default () => {
+    const screen = document.createElement('section');
+    const templateScreen = `
+    <section>
+        <header>
+            <img src='img/logoRaízen.png' class='logoCostumer' alt='Logo Raízen'>
+        </header>
+    </section>
+    <input type="text" placeholder="Digite seu CNPJ" id="input-cnpj"></input>
+      <button id="btn-search" class="search-btn">Buscar por CNPJ</button>
+      <div id='query-template'> 
+      </div>
+    `;
+
+    screen.innerHTML = templateScreen;
+
+    const btnSearch = screen.querySelector("#btn-search");
+    const inputCnpj = screen.querySelector("#input-cnpj");
+    const printElement = screen.querySelector('#query-template')
+    btnSearch.addEventListener('click', () => {
+        const filterCnpj = filterInfo(companies, inputCnpj.value)
+        printElement.innerHTML = createCard(filterCnpj);
+    })
+
+    return screen;
+}
+
+function createCard(filterCnpj) {
+    const companiesArray = filterCnpj.map((infos) => {
         const template = `
          <section>
+            <strong>CNPJ:</strong> ${infos.empresa}</br>
+             <strong>Lote:</strong> ${infos.lote}</br>
              <strong>Localização:</strong> ${infos.localização}</br>
              <strong>Procedência:</strong> ${infos.procedência}</br>
-             <strong>Lote:</strong> ${infos.lote}</br>
              <strong>Açúcar:</strong> ${infos.açúcar}</br>
-             <strong>Data:</strong> ${infos.data}</br>
+             <strong>Data de Venda:</strong> ${infos.data}</br>
          </section>
      `;
 
@@ -19,21 +47,9 @@ function createCard(companies) {
     return companiesArray.join("");
 }
 
-export default () => {
-    const screen = document.createElement('section');
-    const templateScreen = `
-    <section>
-        <header>
-            <img src='img/logoRaízen.png' class='logoCostumer' alt='Logo Raízen'>
-        </header>
-        <div id='query-template'>
-        ${createCard(companies)}
-    </div>
-    </section>
-    <input type="text" placeholder="Digite seu CNPJ" class="input-cnpj">
-      <button id="btn-search" class="search-btn">Buscar por CNPJ</button>
-    `;
-
-    screen.innerHTML = templateScreen;
-    return screen;
+function filterInfo(companies, cnpj,) {
+    const arrayInfo = companies.filter((infos) => infos.cnpj === cnpj);
+    return arrayInfo;
 }
+
+
